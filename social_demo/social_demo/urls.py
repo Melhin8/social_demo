47385b9bd97e1737995998d64f67a4eade5f4ac6
melhin8 <melhin8@gmail.com>
2022-05-09 12:06:16 +0300

    set up urls

--- social_demo/social_demo/urls.py
+++ social_demo/social_demo/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from post.views import UserViewSet, SignupUserView, PostViewSet, LikeViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'posts', PostViewSet)
+router.register(r'likes', LikeViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('auth/signup/', SignupUserView.as_view()),
 ]
