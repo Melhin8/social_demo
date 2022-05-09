6ffdefc0200eabe436d7fd4326c34ee6bafa716e
melhin8 <melhin8@gmail.com>
2022-05-09 11:28:55 +0300

    add IsAuthorPermission

--- social_demo/post/views.py
+++ social_demo/post/views.py
@@ -6,6 +6,7 @@ from rest_framework.generics import CreateAPIView
 from post.models import Post, Like
 from post.serializers import UserSerializer, PostSerializer, LikeSerializer
 from post.mixins import AutofillAuthorMixin
+from post.permissions import IsAuthorPermission
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -22,10 +23,10 @@ class SignupUserView(CreateAPIView):
 class PostViewSet(AutofillAuthorMixin, viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (permissions.IsAuthenticated)
+    permission_classes = (permissions.IsAuthenticated, IsAuthorPermission)
 
 
 class LikeViewSet(AutofillAuthorMixin, viewsets.ModelViewSet):
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
-    permission_classes = (permissions.IsAuthenticated)
+    permission_classes = (permissions.IsAuthenticated, IsAuthorPermission)
