9af1b6125461f0c624e7113bd8e8239e52a56704
melhin8 <melhin8@gmail.com>
2022-05-09 10:40:30 +0300

    add view

--- social_demo/post/views.py
+++ social_demo/post/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets, permissions
+from rest_framework.permissions import AllowAny
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from post.models import Post, Like
+from post.serializers import UserSerializer, PostSerializer, LikeSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class SignupUserView(CreateAPIView):
+    model = User
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticated)
+
+
+class LikeViewSet(viewsets.ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    permission_classes = (permissions.IsAuthenticated)
